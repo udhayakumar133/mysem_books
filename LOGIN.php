4404309eac5e51999943a9c74cdb1b604eeadf8d
udhayakumar133 <76897420+udhayakumar133@users.noreply.github.com>
2021-01-04 16:21:58 +0530

    delete home.php

--- LOGIN.php
+++ LOGIN.php
@@ -124,7 +124,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="./css/LOGIN.css" />
+    <link rel="stylesheet" type="text/css" href="./css/login.css" />
     <title>Document</title>
 </head>
 <body>
