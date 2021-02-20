ecc072c3135328c7803acde00ed9e2a38632cbac
udhayakumar133 <76897420+udhayakumar133@users.noreply.github.com>
2021-02-20 22:54:08 +0530

    Adding admin panel and styles

---
+++ header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/header.css" />
+    <style>
+       
+        .container {
+            width: 100%;
+            height: 60%;
+            background: green;
+        }
+
+        .container .left-c {
+            height: 50%;
+            width: 30%;
+            background: red;
+
+        }
+    </style>
+    <title>Document</title>
+
+</head>
+<body>
+    <nav>
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn">
+            <i class="fas fa-bars"></i>
+        </label>
+        <label class="logo">Mysem BOOKS</label>
+        <ul>
+            <li><a href="#" class="active">HOME</a></li>
+            <li><a href="#">OLD BOOKS</a></li>
+            <li><a href="#">Recommend</a></li>
+            <li><a href="#">LOGIN</a></li>
+            <li><a href="#">MY ACCOUNT</a></li>
+        </ul>
+    </nav>
+    <div class="container">
+        <div class="left-c">
+
+
+        </div>
+        <div class="right-c">
+
+        </div>
+    </div>
+</body>
+</html>
